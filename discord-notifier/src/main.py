6181f7284e806d876eaf1b41a69c840c53d60a91
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2025-07-18 08:43:49 +0100

    Add some stuff

--- discord-notifier/src/main.py
+++ discord-notifier/src/main.py
@@ -1,8 +1,16 @@
 """Module to handle discordn-notifier"""
 
 import discord
+import dotenv
+import os
 
 
+dotenv.load_dotenv()
 
+bot_token = str(os.getenv("TOKEN"))
+
+notifier: discord.Bot = discord.Bot(  # type: ignore[no-untyped-call]
+    intents=discord.Intents.default(),
+)
 
 
